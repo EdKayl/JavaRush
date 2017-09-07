7774bc0c55e12e13bb299169fd5e0100cabe781e
EdKayl <edk2005@yandex.ru>
2017-09-07 12:43:16 +0300

    2411 done

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2411/C.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2411/C.java
@@ -1,7 +1,5 @@
 package com.javarush.task.task24.task2411;
 
-import com.javarush.task.task21.task2109.Solution;
-
 public class C implements JustAnInterface {
     public C() {
         System.out.print("C");
